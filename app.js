90eb4a2f405585a715a7ba1900fe8d20559024a8
tusharnikam1 <tushar.nikam@globant.com>
2019-10-25 11:40:00 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const redis = require("redis");
+const MongoClient = require("mongodb").MongoClient;
+
+const client = redis.createClient({ port: 6379, host: "cache" });
+
+client.on("connect", () => {
+  console.log("Redis connected");
+});
+
+client.on("error", error => {
+  console.log(`Error connecting redis : ${error}`);
+});
+
+// Connecting mongodb
+const url = "mongodb://mongo:27017";
+MongoClient.connect(url, (err, client) => {
+  if (err) console.error(err, "Error connecting mongodb");
+  console.log(client, "MongoDB connected");
+});
